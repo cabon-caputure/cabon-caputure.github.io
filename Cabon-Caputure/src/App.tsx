7e29dff45771dd936953f2f14a851d779b12c166
Gamer-Guy12 <123429926+Gamer-Guy12@users.noreply.github.com>
2023-05-24 18:10:33 +0000

    hi

--- Cabon-Caputure/src/App.tsx
+++ Cabon-Caputure/src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar"
 import './App.css'
 import HomePage from "./pages/HomePage"
+import Animation from "./pages/Animation"
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />}/>
+        <Route path="/animation" element={<Animation />} />
       </Routes>
       <Navbar />
     </>
---
+++ Cabon-Caputure/src/pages/Animation.tsx
@@ -0,0 +1,7 @@
+export default function Animation() {
+
+    return (
+        <>Dumb</>
+    )
+
+}
