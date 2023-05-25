e74f5d0f2dab6e6cb7106cc5369b2fd08451cf4c
Gamer-Guy12 <123429926+Gamer-Guy12@users.noreply.github.com>
2023-05-25 05:52:13 +0000

    w

---
+++ Cabon-Caputure/src/pages/game.tsx
@@ -0,0 +1,11 @@
+import "./Ref.css"
+
+export default function Game() {
+
+    return (
+
+        <><iframe src="https://cabon-caputure.github.io/video-game/" className="ref"></iframe></>
+
+    )
+
+}
