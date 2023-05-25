bdb615415f660402f9959872e14372ffc18fc2e7
Gamer-Guy12 <123429926+Gamer-Guy12@users.noreply.github.com>
2023-05-25 05:41:08 +0000

    hi

--- Cabon-Caputure/src/pages/Animation.tsx
+++ Cabon-Caputure/src/pages/Animation.tsx
@@ -1,7 +1,11 @@
+import "./Ref.css"
+
 export default function Animation() {
 
     return (
-        <>Dumb</>
+        <>
+            <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vT276UvVNVzvoTEktG-W4Fioo4rgtmjWS6X-ExejUIDszXemWHTpcWr-877CFTBEa3gzgCW-bucr9Ug/pub?start=true&loop=false&delayms=75" className="ref"></iframe>
+        </>
     )
 
 }
